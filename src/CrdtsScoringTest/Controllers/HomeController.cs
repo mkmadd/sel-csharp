6012a4fc643f7d304e2729144a74fb2c1b1b1ddd
mkmadd <mkmadd@gmail.com>
2016-10-20 21:56:05 -0500

    Initial commit

---
+++ src/CrdtsScoringTest/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using OpenQA.Selenium;
+using CrdtsScoringTestLib;
+
+namespace CrdtsScoringTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //IList<IWebElement> results;
+            IWebElement result;
+            using (var examiner = new Scoring())
+            {
+                examiner.loadUrl("https://demo.p1dev.net/crdtsdemo");
+                result = examiner.test1();
+            }
+            return View(result);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/CrdtsScoringTestLib/Scoring.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+namespace CrdtsScoringTestLib
+{
+    public class Scoring : IDisposable
+    {
+        private IWebDriver driver;
+
+        public Scoring()
+        {
+            driver = new InternetExplorerDriver();
+        }
+
+        public void wait(int seconds)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+        }
+
+        public void loadUrl(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public IList<IWebElement> test()
+        {
+            return driver.FindElements(By.ClassName("ChildSelectedItem"));
+        }
+
+        public IWebElement test1()
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            return wait.Until(ExpectedConditions.ElementIsVisible(By.Id("aspnetForm")));
+        }
+
+        public void Dispose()
+        {
+            driver.Dispose();
+        }
+    }
+}
